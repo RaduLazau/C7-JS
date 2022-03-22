0d92c3e7cff57e289be9f2dfe6df0237ddab8d03
Radu L <lazauradu@gmail.com>
2022-03-22 18:49:29 +0200

    add first assign

---
+++ scripta.js
@@ -0,0 +1,22 @@
+/*function myFunction() {
+    alert("I am an alert box!");
+}
+*/
+//console.log(myFunction);
+
+/*window.confirm('Confirmation');
+    if(confirm("Press a button!")){
+        txt = "You pressed OK!";
+    } else {
+        txt = "You pressed Cancel!";
+    }
+
+console.log('Confirmation')*/
+
+const myMovie = prompt("What's your favourite movie?");
+    if (myMovie === "Batman") {
+    console.log(`My favourite movie is, ${myMovie}`);
+    }
+    else {
+        console.log(myMovie);
+    }
